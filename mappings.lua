50721fc1b8f4f778bca71ac03ebcc40ee30ab267
Daniel Higa <danielhiga10@gmail.com>
2023-05-28 08:51:51 -0400

    Add vim-tmux-navigator

--- mappings.lua
+++ mappings.lua
@@ -10,6 +10,11 @@ M.disabled = {
 
 M.general = {
   n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+
     ["<leader>q"] = { "<cmd> q <CR>", "close" },
 
     ["<leader>fm"] = {

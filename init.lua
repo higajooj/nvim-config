9ce03957c85fdd44bedf04356683665bf94afefa
Daniel Higa <danielhiga10@gmail.com>
2023-05-28 08:51:38 -0400

    Fix WSL clipboard integration with nvim

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
 vim.opt.colorcolumn = "80"
+
+in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+
+if in_wsl then
+    vim.g.clipboard = {
+        name = 'wsl clipboard',
+        copy =  { ["+"] = { "clip.exe" },   ["*"] = { "clip.exe" } },
+        paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+
+        cache_enabled = true
+
+    }
+end

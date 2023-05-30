2256e334651e596f0163dfad5284554d20d771cc
Daniel Higa <danielhiga10@gmail.com>
2023-05-30 15:43:35 -0400

    Add html snippets to .tsx

--- plugins.lua
+++ plugins.lua
@@ -1,73 +1,81 @@
 local plugins = {
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "stylua",
-        "typescript-language-server",
-        "pyright",
-        "prisma-language-server",
-        "rust-analyzer",
-        "prettier",
-        "htmlbeautifier",
-        "standardrb",
-        "flake8",
-        "black",
-        "rustfmt",
-      },
-    },
-  },
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"stylua",
+				"typescript-language-server",
+				"pyright",
+				"prisma-language-server",
+				"rust-analyzer",
+				"prettier",
+				"htmlbeautifier",
+				"standardrb",
+				"flake8",
+				"black",
+				"rustfmt",
+			},
+		},
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "rust",
-        "javascript",
-        "typescript",
-        "tsx",
-        "c",
-        "cpp",
-        "rust",
-        "html",
-        "css",
-        "json",
-        "vue",
-        "svelte",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"rust",
+				"javascript",
+				"typescript",
+				"tsx",
+				"c",
+				"cpp",
+				"rust",
+				"html",
+				"css",
+				"json",
+				"vue",
+				"svelte",
+			},
+		},
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require "custom.configs.null-ls"
-      end,
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
 
-  {
-    "andweeb/presence.nvim",
-    lazy = false,
-    config = function()
-      require("presence").setup {
-        show_time = false,
-        buttons = false,
-        neovim_image_text = "GIGACHAD TEXT EDITOR",
-      }
-    end,
-  },
+	{
+		"L3MON4D3/LuaSnip",
+		config = function(_, opts)
+			require("plugins.configs.others").luasnip(opts)
+			require("luasnip").filetype_extend("typescriptreact", { "html" })
+		end,
+	},
+
+	{
+		"andweeb/presence.nvim",
+		lazy = false,
+		config = function()
+			require("presence").setup({
+				show_time = false,
+				buttons = false,
+				neovim_image_text = "GIGACHAD TEXT EDITOR",
+			})
+		end,
+	},
 }
 
 return plugins

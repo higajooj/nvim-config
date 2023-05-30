d3345560d2e3cc5f22734c6259d44ff854d72b31
Daniel Higa <danielhiga10@gmail.com>
2023-05-30 15:43:17 -0400

    Format init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,13 @@
 vim.opt.colorcolumn = "80"
 
-in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
 
 if in_wsl then
-    vim.g.clipboard = {
-        name = 'wsl clipboard',
-        copy =  { ["+"] = { "clip.exe" },   ["*"] = { "clip.exe" } },
-        paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+	vim.g.clipboard = {
+		name = "wsl clipboard",
+		copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
+		paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
 
-        cache_enabled = true
-
-    }
+		cache_enabled = true,
+	}
 end

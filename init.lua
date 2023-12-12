6422dfb4de928d6847f823c83399a99ae3004b08
Daniel Higa <danielhiga10@gmail.com>
2023-12-12 12:13:39 -0400

    Remove wsl snippet

--- init.lua
+++ init.lua
@@ -1,17 +1,5 @@
 vim.opt.colorcolumn = "80"
 
-in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
-
-if in_wsl then
-	vim.g.clipboard = {
-		name = "wsl clipboard",
-		copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
-		paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
-
-		cache_enabled = true,
-	}
-end
-
 vim.cmd([[
    augroup ilikecursorline
       autocmd VimEnter * :highlight CursorLine guibg=#282a2e

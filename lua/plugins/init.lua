cd210c9d3ca8dd5d7f1bf22b44439ad09e7ed1c3
Daniel Higa <danielhiga10@gmail.com>
2025-05-11 20:08:55 -0400

    Add avante

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,15 @@
 return {
-  { "williamboman/mason.nvim", enabled = false },
+  {
+    "williamboman/mason.nvim",
+    enabled = false,
+  },
+
+  {
+    "NvChad/base46",
+    branch = "v3.0",
+  },
+
+  { import = "nvchad.blink.lazyspec" },
 
   { "christoomey/vim-tmux-navigator", lazy = false },
 
@@ -74,6 +84,61 @@ return {
       show_time = false,
       buttons = false,
       neovim_image_text = "GIGACHAD TEXT EDITOR",
+    },
+  },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      provider = "openai",
+      openai = {
+        model = "gpt-4o-mini",
+      },
+      -- cursor_applying_provider = 'groq',     -- In this example, use Groq for applying, but you can also use any provider you want.
+      behaviour = {
+        enable_cursor_planning_mode = true,
+      },
+    },
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+      -- {
+      --   -- support for image pasting
+      --   "HakonHarnes/img-clip.nvim",
+      --   event = "VeryLazy",
+      --   opts = {
+      --     -- recommended settings
+      --     default = {
+      --       embed_image_as_base64 = false,
+      --       prompt_for_file_name = false,
+      --       drag_and_drop = {
+      --         insert_mode = true,
+      --       },
+      --       -- required for Windows users
+      --       use_absolute_path = true,
+      --     },
+      --   },
+      -- },
+      -- {
+      --   -- Make sure to set this up properly if you have lazy=true
+      --   "MeanderingProgrammer/render-markdown.nvim",
+      --   opts = {
+      --     file_types = { "markdown", "Avante" },
+      --   },
+      --   ft = { "markdown", "Avante" },
+      -- },
     },
   },
 }
